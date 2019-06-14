25eaf6f11b324b8c84bf963cf74acdb1012ecc47
Zuhair Haroon Khan <zk222ac@gmail.com>
2019-06-14 19:53:23 +0200

    Unit Test Account

---
+++ TestAccountUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAccountUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAccountUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("649d06f7-a49e-40ef-927b-1e571c4a9727")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestAccountUnit/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using AccountTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAccountUnit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private double epsilon = 1e-6;
+        [TestMethod]
+        public void AccountCannotHaveNegativeOverdraftLimit()
+        {
+            Account account = new Account(-20);
+            Assert.AreEqual(0, account.OverdraftLimit, epsilon);
+        }
+
+        [TestMethod]
+       //The Deposit methods will not accept negative numbers.
+        public void DepositDoesNotAcceptNegativeValue()
+        {
+            Account account = new Account(20);
+            bool result = account.Deposit(-10);
+            Assert.AreEqual(0, account.GetBalance(), epsilon);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        //The WithDraw methods will not accept negative numbers.
+        public void WithDrawDoesNotAcceptNegativeValue()
+        {
+            Account account = new Account(20);
+            bool result = account.Withdraw(-10);
+            Assert.AreEqual(0, account.GetBalance(), epsilon);
+            Assert.IsFalse(result);
+        }
+
+        //  Account cannot overstep its overdraft limit.
+        [TestMethod]
+        public void AccountCanNotOverStepOverDraftLimit()
+        {
+            Account account = new Account(20);
+            bool result = account.Withdraw(70);
+            Assert.AreEqual(0, account.GetBalance(), epsilon);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        // The Deposit and Withdraw methods will deposit or withdraw the correct amount, respectively.
+        public void DepositPositiveValueOk()
+        {
+            Account account = new Account(20);
+            bool result = account.Deposit(10);
+            Assert.AreEqual(10, account.GetBalance(), epsilon);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void WithDrawPositiveValueOverDraftOk()
+        {
+            Account account = new Account(20);
+            bool result = account.Withdraw(10);
+            Assert.AreEqual(-10, account.GetBalance(), epsilon);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void WithDrawPositiveValueBalanceOk()
+        {
+            Account account = new Account(0);
+            account.Deposit(30);
+            bool result = account.Withdraw(10);
+            Assert.AreEqual(20, account.GetBalance(), epsilon);
+            Assert.IsTrue(result);
+        }
+
+
+    }
+}
